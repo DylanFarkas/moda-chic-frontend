f4ea71cd2145d7fb0d842248d2b447c1c344444a
m-caicedo <macaicedo03@gmail.com>
2025-05-02 17:00:27 -0500

    opcion de filtrado y busqueda

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
-import { useNavigate } from 'react-router-dom';
-import Carousel from '../../components/Carousel/Carousel';
-import Navbar from '../../components/Navbar/Navbar';
-import ProductCards from '../../components/ProductCards/ProductCards';
-import './Home.css'
+import { useNavigate } from "react-router-dom";
+import Carousel from "../../components/Carousel/Carousel";
+import Navbar from "../../components/Navbar/Navbar";
+import ProductCards from "../../components/ProductCards/ProductCards";
+import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -12,16 +12,23 @@ const Home = () => {
       <Navbar />
       <Carousel />
 
-      <section className='products-section'>
+      <section className="products-section">
         <h2 className="section-title"> Nuestra Colección</h2>
-        <div className="buttons">
-          <button onClick={() => navigate('/pantalon-page?categoria=pantalones')} className='button-category'>
-            Pantalones
-          </button>
-          <button onClick={() => navigate('/camisa-page?categoria=camisas')} className='button-category'>
-            Camisas
-          </button>
-
+        <div className="category-icons">
+          <div
+            className="category-item"
+            onClick={() => navigate("/pantalon-page?categoria=pantalones")}
+          >
+            <img src="/icons/pantalon-icon.png" alt="Pantalones" />
+            <span>Pantalones</span>
+          </div>
+          <div
+            className="category-item"
+            onClick={() => navigate("/camisa-page?categoria=camisas")}
+          >
+            <img src="/icons/camisa-icon.png" alt="Camisas" />
+            <span>Camisas</span>
+          </div>
         </div>
         <ProductCards />
       </section>
